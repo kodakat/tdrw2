81cd44ec7355f47c6ae17daf44ddac09edd0ca8d
Toshihiro Kodaka <kodaka@me.com>
2012-05-01 08:22:07 +0900

    first commit

--- src/main/java/tdrw/TdrwMain.java
+++ src/main/java/tdrw/TdrwMain.java
@@ -4,10 +4,11 @@ package tdrw;
  * Hello world!
  *
  */
-public class App 
+public class TdrwMain
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
     }
 }
